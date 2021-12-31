9496534aa7cc82531240d572927985ec1a53fba7
Cmart88 <ftballplaya1221@gmail.com>
2021-12-31 15:38:29 -0800

    changes to app.js for reviews

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const express = require('express'),
 	app = express(),
 	mongoose = require('mongoose'),
 	methodOverride = require('method-override'),
-	Campground = require('./models/campground'),
 	morgan = require('morgan'),
 	ejsMate = require('ejs-mate'),
 	catchAsync = require('./utils/catchAsync'),
 	ExpressError = require('./utils/ExpressError'),
-	{ campgroundSchema, reviewSchema } = require('./schemas.js'),
-	Review = require('./models/review'),
 	campgrounds = require('./routes/campground');
+reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser    : true,
@@ -30,44 +28,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
 
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map((el) => el.message).join(',');
-		throw new ExpressError(msg, 400);
-	}
-	else {
-		next();
-	}
-};
-
 app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
-app.post(
-	'/campgrounds/:id/reviews',
-	validateReview,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findById(id);
-		const review = new Review(req.body.review);
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-app.delete(
-	'/campgrounds/:id/reviews/:reviewId',
-	catchAsync(async (req, res) => {
-		const { id, reviewId } = req.params;
-		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-		await Review.findByIdAndDelete(reviewId);
-		res.redirect(`/campgrounds/${id}`);
-	})
-);
 
 app.all('*', (req, res, next) => {
 	next(new ExpressError('Page not found!!', 404));
