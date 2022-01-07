f61c718bdfc637e4384e166251e3f33ebcdd1517
Cmart88 <ftballplaya1221@gmail.com>
2022-01-07 05:52:25 -0800

    updated registration

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,9 @@ router.post(
 			const { username, email, password } = req.body;
 			const user = new User({ email, username });
 			const registeredUser = await User.register(user, password);
-			console.log(registeredUser);
+			req.login(registeredUser, (err) => {
+				if (err) return next(err);
+			});
 			req.flash('success', 'Welcome to Yelp Camp!');
 			res.redirect('/campgrounds');
 		} catch (e) {
