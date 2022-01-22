c958de448f1b86f7aa217df277e8c5c99412819e
Cmart88 <ftballplaya1221@gmail.com>
2022-01-22 14:54:45 -0800

    added mongo sanitize

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+    require("dotenv").config();
 }
 console.log(process.env.API_KEY);
 const express = require("express");
@@ -18,15 +18,16 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const User = require("./models/user");
+const mongoSanitize = require("express-mongo-sanitize");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database connected");
+    console.log("Database connected");
 });
 
 app.engine("ejs", ejsMate);
@@ -34,19 +35,20 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // app.use(morgan('dev'));
+app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
-  secret: "thisshouldbeabettersecret!",
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-  },
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
 };
 app.use(session(sessionConfig));
 app.use(flash());
@@ -58,11 +60,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  // console.log(req.session);
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
+    // console.log(req.session);
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 });
 
 app.use("/", userRoutes);
@@ -70,18 +72,18 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
-  res.render("home");
+    res.render("home");
 });
 
 app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found!!", 404));
+    next(new ExpressError("Page not found!!", 404));
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = "Oh no something went wrong!";
-  res.status(statusCode).render("error", { err });
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Oh no something went wrong!";
+    res.status(statusCode).render("error", { err });
 });
 app.listen(3000, () => {
-  console.log("server running on port 3000");
+    console.log("server running on port 3000");
 });
